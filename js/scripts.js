0c07b6cd73a8ca2ead92710a4e6bce3f331f6e58
jessica engel & Courtney Phillips <student@epicodus.com>
2015-05-26 10:13:18 -0700

    Return true for equilateral triangle. Test Pass.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var triangleChecker = function(side1, side2, side3){
+  if ((side1 === side2) && (side1 == side3)) {
+    return true;
+  }
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('triangleChecker', function() {
+  it("returns the type of triangle three values create", function(){
+    expect(triangleChecker(10,10,10)).to.equal(true);
+  });
+});
