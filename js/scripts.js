020ab7c69b5dec152a3931d4fc40cec6df2b6c18
jessica engel & Courtney Phillips <student@epicodus.com>
2015-05-26 10:42:03 -0700

    Have ability to check for Scalene and Isosceles triangles. Tests pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
 var triangleChecker = function(side1, side2, side3){
   if ((side1 === side2) && (side1 == side3)) {
-    return true;
+    return "Equilateral"
+  }
+  else if ((side1 !== side2) && (side2 !== side3) && (side1 !== side3)) {
+    return "Scalene"
+  }
+
+  else if ((side1 === side2) || (side1 === side3) || (side2 === side3)) {
+    return "Isosceles"
   }
 };
+
+
+$(document).ready(function(){
+
+  $("form.triangle-form").submit(function(event){
+    side1 = parseInt($(".side1").val())
+    side2 = parseInt($(".side2").val())
+    side3 = parseInt($(".side3").val())
+
+    result = triangleChecker(side1, side2, side3)
+
+    if (result === "Equilateral"){
+    $(".answer").text("Equilateral");
+  }
+
+    if (result === "Scalene"){
+    $(".answer").text("Scalene");
+  }
+
+    event.preventDefault();
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,14 @@
 describe('triangleChecker', function() {
-  it("returns the type of triangle three values create", function(){
-    expect(triangleChecker(10,10,10)).to.equal(true);
+  it("returns equilateral when 3 sides with same size are input", function(){
+    expect(triangleChecker(10,10,10)).to.equal("Equilateral");
   });
+
+  it("returns scalene when 3 sides with different sizes are input", function(){
+    expect(triangleChecker(10,11,12)).to.equal("Scalene");
+  });
+
+  it("returns isosceles when 2 sides with same sizes are input", function(){
+    expect(triangleChecker(10,11,10)).to.equal("Isosceles");
+  });
+
 });
